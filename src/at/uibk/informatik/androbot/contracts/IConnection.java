7eb820612e6041c34362d83caff31a57fc628f08
adanek <adanek@debian.home.local>
2015-03-14 00:05:02 +0100

    BluetoothConnection implemented

--- src/at/uibk/informatik/androbot/contracts/IConnection.java
+++ src/at/uibk/informatik/androbot/contracts/IConnection.java
@@ -1,8 +1,10 @@
 package at.uibk.informatik.androbot.contracts;
 
+import java.io.IOException;
+
 public interface IConnection {
 
      void write(byte[] message);
-     void connect();
+     void connect() throws IOException;
      void disconnect();     
 }
--- src/at/uibk/informatik/androbot/control/BluetoothConnection.java
+++ src/at/uibk/informatik/androbot/control/BluetoothConnection.java
@@ -1,8 +1,9 @@
 package at.uibk.informatik.androbot.control;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.UUID;
+import java.lang.reflect.Method;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -12,13 +13,22 @@ import at.uibk.informatik.androbot.contracts.IConnection;
 
 public class BluetoothConnection implements IConnection {
 
-	private BluetoothAdapter btAdapter;
-	private BluetoothSocket btSocket;
-	private OutputStream outStream;
+	// **********************************************************************************************
+	// Locals
+	// **********************************************************************************************
 
-	private static final UUID SERVICE_ID = UUID.randomUUID();
+	private BluetoothAdapter btAdapter;
+
+	//private static final UUID SERVICE_ID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 	private static final String TAG = "BluetoothConnenction";
 	private String address;
+
+	private ConnectThread tConnect;
+	private BtDataExchangeThread tConnected;
+
+	// **********************************************************************************************
+	// Constructors
+	// **********************************************************************************************
 
 	public BluetoothConnection(String macAddress) {
 		this.btAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -26,75 +36,61 @@ public class BluetoothConnection implements IConnection {
 		this.setAddress(macAddress);
 	}
 
-	@Override
-	public void write(byte[] msg) {
-		
-		Log.d(TAG, "Sending message: " + msg);
-		try {
-			outStream.write(msg);
-		} catch (IOException e) {
-			Log.d(TAG, "Exception occured during writing: " + e.getMessage());
-		}
+	// **********************************************************************************************
+	// Properties
+	// **********************************************************************************************
+
+	public String getAddress() {
+		return address;
 	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	// **********************************************************************************************
+	// Methods
+	// **********************************************************************************************
 
 	@Override
 	public void connect() {
+		// Ensure Bluetooth is enabled
+		this.checkBtState();
 
 		// Set pointer to the remote device
 		BluetoothDevice device = btAdapter.getRemoteDevice(getAddress());
 
-		try {
-			btSocket = createSocket(device);
-			
-		} catch (IOException e) {
-			Log.d(TAG, "Can't create socket to remote device");
-		}
+		// Check if another thread is running
+		if (this.tConnect != null && this.tConnect.isAlive())
+			this.tConnect.cancel();
 
-		// Cancel discovery before opening socket
-		btAdapter.cancelDiscovery();
-
-		// Open socket
-		try {
-			Log.d(TAG, "Try do open socket to remote device...");
-			btSocket.connect();
-			Log.d(TAG, "Connection established");
-		} catch (IOException e) {
-			try {
-				Log.d(TAG, "Connection failed");
-				btSocket.close();
-			} catch (IOException e_closing) {
-				Log.d(TAG, "Can't close socket: " + e_closing.getMessage());
-			}
-		}
-		
-		// Create output stream
-		try{
-			Log.d(TAG, "Creating stream...");
-			outStream = btSocket.getOutputStream();
-			Log.d(TAG, "OutputStream ready");
-		}catch(IOException e){
-			Log.d(TAG, "Stream creation failed: "+ e.getMessage());			
-		}
+		// Create a new Thread which tries to connect to the device
+		this.tConnect = new ConnectThread(device);
+		this.tConnect.start();
 	}
 
 	@Override
-	public void disconnect(){
-		
-		if(outStream != null){
-			try {
-				outStream.flush();
-			} catch (IOException e) {
-				Log.d(TAG, "Failed to flush the outputstream: " + e.getMessage());
-			}
-		}
-		
-		try {
-			btSocket.close();
-		} catch (IOException e) {
-			Log.d(TAG, "Faild to close the socket: " + e.getMessage());
-		}
+	public void disconnect() {
+		// Cancel connect thread
+		if (this.tConnect != null && this.tConnect.isAlive())
+			this.tConnect.cancel();
+
+		// Cancel connected thread
+		if (this.tConnected != null && this.tConnected.isAlive())
+			this.tConnected.cancel();
 	}
-	
+
+	@Override
+	public void write(byte[] msg) {
+
+		if (this.tConnected == null || !this.tConnected.isAlive())
+			throw new IllegalStateException(
+					"The connection must be connected before writing");
+
+		Log.d(TAG, "Sending message: " + msg);
+		this.tConnected.write(msg);
+	}
+
 	private void checkBtState() {
 
 		// Ensure Bluetooth is supported
@@ -109,17 +105,159 @@ public class BluetoothConnection implements IConnection {
 
 	}
 
-	private BluetoothSocket createSocket(BluetoothDevice device)
-			throws IOException {
+	private void manageConnectedSocket(BluetoothSocket socket) {
 
-		return device.createRfcommSocketToServiceRecord(SERVICE_ID);
+		Log.d(TAG, "Connection established");
+
+		this.tConnected = new BtDataExchangeThread(socket);
+		this.tConnected.start();
 	}
 
-	public String getAddress() {
-		return address;
+	// **********************************************************************************************
+	// Helper Classes
+	// **********************************************************************************************
+
+	// Encapsulates the connection to the remote device in own thread
+	private class ConnectThread extends Thread {
+
+		private final BluetoothSocket btSocket;
+		private final String TAG = "BtConnectThread";
+
+		public ConnectThread(BluetoothDevice device) {
+			// tmp variable because btSocket is final (Hint from android
+			// documentation)
+			BluetoothSocket socket = null;
+
+			try {
+				// Doesn't work for some reason
+				// socket =
+				// device.createRfcommSocketToServiceRecord(SERVICE_ID);
+
+				// Workaround found on StackOverflow
+				Method m;
+				m = device.getClass().getMethod("createRfcommSocket",
+						new Class[] { int.class });
+				socket = (BluetoothSocket) m.invoke(device, Integer.valueOf(1));
+			} catch (Exception e) {
+			}
+
+			btSocket = socket;
+			Log.d(TAG, "Thread created");
+		}
+
+		public void run() {
+
+			Log.d(TAG, "Thread started");
+
+			// The discovery should be deactivated before opening a socket
+			btAdapter.cancelDiscovery();
+
+			try {
+				Log.d(TAG, "Try to open a socket to the remote device");
+				btSocket.connect();
+
+			} catch (IOException connectException) {
+				try {
+					Log.d(TAG,
+							"Socket connection failed:\n"
+									+ connectException.getMessage());
+					btSocket.close();
+				} catch (IOException closeException) {
+					Log.d(TAG, "Closing socket failed");
+				}
+				return;
+			}
+
+			Log.d(TAG, "Socket connected");
+			manageConnectedSocket(btSocket);
+		}
+
+		public void cancel() {
+
+			Log.d(TAG, "Connection canceld");
+			try {
+				btSocket.close();
+			} catch (IOException e) {
+				Log.d("ConnectThread", "Failed to close socket");
+			}
+		}
 	}
 
-	public void setAddress(String address) {
-		this.address = address;
+	// Encapsulates the data exchange with the remote device
+	private class BtDataExchangeThread extends Thread {
+
+		private final String LOG_TAG = "BtDataExchangeThread";
+		private final BluetoothSocket btSocket;
+		private final InputStream btInStream;
+		private final OutputStream btOutStream;
+
+		public BtDataExchangeThread(BluetoothSocket socket) {
+			this.btSocket = socket;
+
+			// Use temporary objects because the lager ones are final
+			InputStream in = null;
+			OutputStream out = null;
+
+			try {
+				in = socket.getInputStream();
+				out = socket.getOutputStream();
+			} catch (IOException e) {
+				Log.d(LOG_TAG,
+						"Unable to get stream from socket: \n" + e.getMessage());
+			}
+
+			btInStream = in;
+			btOutStream = out;
+
+			Log.d(LOG_TAG, "Thread created");
+		}
+
+		// Receives messages from remote device
+		public void run() {
+
+			byte[] buffer = new byte[1024];
+			int bytes;
+
+			Log.d(LOG_TAG, "Start to listen on socket for incoming messages");
+
+			while (true) {
+				try {
+					bytes = btInStream.read(buffer);
+
+					// TODO: Send message to caller
+				} catch (IOException e) {
+					break;
+				}
+			}
+
+			Log.d(LOG_TAG, "Thread finished");
+		}
+
+		// Sends a message to the remote device
+		public void write(byte[] data) {
+			Log.d(LOG_TAG, "Writing message: " + data.toString());
+
+			try {
+				btOutStream.write(data);
+			} catch (IOException e) {
+				Log.d(LOG_TAG, "Failed to write data: \n" + e.getMessage());
+			}
+		}
+
+		public void cancel() {
+			Log.d(LOG_TAG, "Thread canceled");
+
+			try {
+				btOutStream.flush();
+			} catch (IOException e) {
+				Log.d(LOG_TAG, "Failed to fluse outgoing stream");
+			}
+
+			try {
+				btSocket.close();
+			} catch (IOException e) {
+				Log.d(LOG_TAG, "Failed to close the connection");
+			}
+		}
 	}
 }
