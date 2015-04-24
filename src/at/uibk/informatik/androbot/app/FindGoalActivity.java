3bbd02beef703676e996e84908adf8903c19e8a6
pati <pati@pati-VirtualBox>
2015-04-24 08:52:50 +0200

    Find_goal

---
+++ src/at/uibk/informatik/androbot/app/FindGoalActivity.java
@@ -0,0 +1,41 @@
+package at.uibk.informatik.androbot.app;
+
+import java.util.List;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import at.uibk.informatik.androbot.control.DistanceSensor;
+import at.uibk.informatik.androbot.control.Position;
+import at.uibk.informatik.androbot.programms.findGoal;
+
+public class FindGoalActivity extends ProgramActivityBase{
+
+	private static final String LOG_TAG = "FindGoalActivity";
+	
+	private findGoal find;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_findgoal);
+		
+		this.find = new findGoal(getApplicationContext());
+		setProgramm(find);
+	}
+	
+	public void onStart(View view){
+		
+		Position target = new Position(0,0,0);
+		
+		find.setTarget(target);
+		
+		find.start();
+	}
+	
+	public void onStop(View view){
+		
+	}
+	
+}
